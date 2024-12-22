a252309d1145e66749c3c7d387895c1771834fe1
willy Chou <willy931118@gmail.com>
2024-12-22 11:40:48 +0800

    殭屍撞到玩家會停下攻擊

--- Assets/Scripts/Zombie AI.cs
+++ Assets/Scripts/Zombie AI.cs
@@ -2,24 +2,80 @@ using UnityEngine;
 
 public class ZombieController : MonoBehaviour
 {
-    public Transform player;
-    public float speed = 3f;
+    public float originalSpeed;
+    public float originalHealth;
+    public Animator animator;
+    public Rigidbody rigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float speed;
+    private float health;
+    private State state;
+
+    private enum State
+    {
+        Walk, 
+        Attack
+    }
     void Start()
     {
-        if (player == null)
-        {
-            Debug.LogError("FollowTarget is not assigned. Please assign it in the Inspector.");
-        }
+        if (animator == null) Debug.Log("Don't find the zombie Animator");
+        if (rigidbody == null) Debug.Log("Don't find the zombie Rigidbody");
+        speed = originalSpeed;
+        health = originalHealth;
+        SetState(State.Walk);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        if (state == State.Walk)
         {
             Vector3 direction = new Vector3(0, 0, 1);
             transform.position += direction * speed * Time.deltaTime;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name != "Plane")
+        {
+            Debug.Log("碰撞開始: " + collision.gameObject.name);
+            speed = 0;
+            SetState(State.Attack);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name != "Plane")
+        {
+            Debug.Log("碰撞結束: " + collision.gameObject.name);
+            speed = originalSpeed;
+            SetState(State.Walk);
+        }
+    }
+
+    private void SetState(State newState)
+    {
+        state = newState;
+        switch (state)
+        {
+            case State.Walk:
+                animator.SetInteger("state", 0);
+                rigidbody.isKinematic = false;
+                rigidbody.constraints = RigidbodyConstraints.FreezeRotation; // 僅鎖定旋轉
+                break;
+            case State.Attack:
+                animator.SetInteger("state", 1);
+                rigidbody.isKinematic = false;
+                rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+                break;
+        }
+        Debug.Log("Zombie State: " + state.ToString());
     }
 }
