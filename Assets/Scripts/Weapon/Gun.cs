8d17e235528a85abee6b256df6483fbfe077ec07
Nues0913 <106960429+Nues0913@users.noreply.github.com>
2024-12-23 15:36:36 +0800

    初步新增了槍枝與射擊， 修改與優化了很多東西， 重構資料夾

---
+++ Assets/Scripts/Weapon/Gun.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform muzzle;
+    public float fireRate = 0.1f;
+
+    private float nextFireTime = 0f;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+        // 可添加子弹特效
+    }
+}
